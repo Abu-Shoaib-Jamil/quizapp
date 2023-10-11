a80b072a25d96d4f1c50f37db00e72e53a55c2f1
Abu Shoaib Jamil <reanimatedshaftain@gmail.com>
2023-10-11 19:49:22 +0530

    Added an api quiz/id by by which user providing the id of the quiz will get all the questions from the quiz and stored in the format of QuestionWrapper

--- src/main/java/com/practice/quizapp/controllers/QuizController.java
+++ src/main/java/com/practice/quizapp/controllers/QuizController.java
@@ -1,11 +1,16 @@
 package com.practice.quizapp.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.practice.quizapp.model.QuestionWrapper;
 import com.practice.quizapp.service.QuizService;
 
 @RestController
@@ -19,5 +24,10 @@ public class QuizController {
 	public String createQuiz(@RequestParam("title") String title,@RequestParam("lang") String lang,@RequestParam("num") int num){
 		quizservice.createQuiz(title,lang,num);
 		return "success";
+	}
+	
+	@GetMapping("{id}")
+	public List<QuestionWrapper> getQuizQuestions(@PathVariable("id") int id) {
+		return quizservice.getQuizQuestionsById(id);
 	}
 }
--- src/main/java/com/practice/quizapp/service/QuizService.java
+++ src/main/java/com/practice/quizapp/service/QuizService.java
@@ -1,11 +1,14 @@
 package com.practice.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.practice.quizapp.model.Question;
+import com.practice.quizapp.model.QuestionWrapper;
 import com.practice.quizapp.model.Quiz;
 import com.practice.quizapp.repo.QuizRepo;
 
@@ -25,5 +28,17 @@ public class QuizService {
 		q.setQuestions(questions);
 		repo.save(q);
 		return "Success";
+	}
+
+	public List<QuestionWrapper> getQuizQuestionsById(int id) {
+		Optional<Quiz> questions = repo.findById(id);
+		List<Question> questionsFromDB = questions.get().getQuestions();
+		List<QuestionWrapper> questionsToClient = new ArrayList<>();
+		for(Question q:questionsFromDB) {
+			
+			QuestionWrapper qw = new QuestionWrapper(q.getQuestionId(),q.getQuestion(),q.getOption1(),q.getOption2(),q.getOption3(),q.getOption4());
+			questionsToClient.add(qw);
+		}
+		return questionsToClient;
 	}
 }
